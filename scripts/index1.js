e36de4e419996295e59ac927cae1981cba4054af
Maria Komarova <maria.komarova@bostongene.com>
2020-10-17 15:56:47 +0300

    feat: change initial cards to template scheme

---
+++ scripts/index1.js
@@ -0,0 +1,48 @@
+const initialCards = [
+  {
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const gridCards = document.querySelector('.places__grid');
+const cardTemplate = document.querySelector("#place").content;
+
+const addCard = (cardData) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardPhoto = cardElement.querySelector('.place__photo');
+  const cardTitle = cardElement.querySelector('.place__title');
+
+  cardPhoto.style.backgroundImage = `url(${cardData.link})`;
+  cardTitle.textContent = cardData.name;
+
+  gridCards.append(cardElement);
+}
+
+initialCards.forEach(cardData => addCard(cardData));
