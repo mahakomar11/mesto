ffc03530d131e869559a17668967455ac7ffcb31
Maria Komarova <maria.komarova@bostongene.com>
2020-10-18 17:50:20 +0300

    feat: add popup with card image

--- scripts/index1.js
+++ scripts/index1.js
@@ -31,19 +31,77 @@ const initialCards = [
   },
 ];
 
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonAdd = document.querySelector('.profile__add-button');
+const buttonsClose = document.querySelectorAll('.popup__close-button');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+
+const togglePopup = (popup) => {
+  popup.classList.toggle('popup_opened');
+}
+
+const closePopup = (event) => {
+  const buttonClose = event.target;
+  const popup = buttonClose.closest('.popup');
+  togglePopup(popup);
+}
+
+buttonsClose.forEach(button => button.addEventListener('click', closePopup));
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const inputName = document.querySelector('.popup__input_content_name');
+const inputJob = document.querySelector('.popup__input_content_job');
+
+const openPopupEditProfile = () => {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+  togglePopup(popupEditProfile);
+}
+
+buttonEdit.addEventListener('click', openPopupEditProfile);
+
+
+const buttonSubmitProfile = popupEditProfile.querySelector('.popup__submit-button');
+
+const submitPopupEditProfile = (event) => {
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  togglePopup(popupEditProfile);
+  event.preventDefault();
+}
+
+buttonSubmitProfile.addEventListener('click', submitPopupEditProfile);
+
+
 const gridCards = document.querySelector('.places__grid');
 const cardTemplate = document.querySelector("#place").content;
 
 const toggleLike = (event) => {
-  buttonLike = event.target;
+  const buttonLike = event.target;
   buttonLike.classList.toggle('place__icon-like_active');
 }
 
 const deleteCard = (event) => {
-  buttonDelete = event.target;
+  const buttonDelete = event.target;
   cardElement = buttonDelete.closest('.place');
   cardElement.remove();
 }
+
+const popupShowCard = document.querySelector('.popup_type_show-card');
+
+const openPopupShowCard = (event) => {
+  const cardPhoto = event.target;
+  const cardTitle = cardPhoto.closest('.place').querySelector('.place__title');
+  const popupPhoto = popupShowCard.querySelector('.popup__photo');
+  const popupCaption = popupShowCard.querySelector('.popup__caption');
+
+  popupPhoto.src = cardPhoto.style.backgroundImage.split('url("')[1].slice(0, -2);
+  popupCaption.textContent = cardTitle.textContent;
+  togglePopup(popupShowCard);
+}
+
+
 const addCard = (cardData) => {
   const cardElement = cardTemplate.cloneNode(true);
   const cardPhoto = cardElement.querySelector('.place__photo');
@@ -52,6 +110,7 @@ const addCard = (cardData) => {
   cardPhoto.style.backgroundImage = `url(${cardData.link})`;
   cardTitle.textContent = cardData.name;
 
+  cardPhoto.addEventListener('click', openPopupShowCard);
   const cardButtonLike = cardElement.querySelector('.place__icon-like');
   cardButtonLike.addEventListener('click', toggleLike);
 
@@ -62,3 +121,5 @@ const addCard = (cardData) => {
 }
 
 initialCards.forEach(cardData => addCard(cardData));
+
+
