d00236ef55d536ba9883f88233a9fdee36df7dae
Maria Komarova <komarova@phystech.edu>
2020-10-21 11:51:57 +0300

    fix: change images to img, rename and move some variables

--- scripts/index.js
+++ scripts/index.js
@@ -30,7 +30,7 @@ const initialCards = [
       "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-const gridCards = document.querySelector('.places__grid');
+const cardsGrid = document.querySelector('.places__grid');
 const cardTemplate = document.querySelector("#place").content;
 // Buttons that open popups
 const buttonEdit = document.querySelector('.profile__edit-button');
@@ -47,32 +47,40 @@ const popupAddCard = document.querySelector('.popup_type_add-card');
 const inputTitle = popupAddCard.querySelector('.popup__input_content_title');
 const inputLink = popupAddCard.querySelector('.popup__input_content_link');
 const buttonSubmitCard = popupAddCard.querySelector('.popup__submit-button');
-// Popup for opening a card
+// Popup for opening a card and it's variables
 const popupShowCard = document.querySelector('.popup_type_show-card');
+const popupPhoto = popupShowCard.querySelector('.popup__photo');
+const popupCaption = popupShowCard.querySelector('.popup__caption');
 // Array with all popups' close buttons
-const buttonsClose = document.querySelectorAll('.popup__close-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
 
 const toggleLike = (event) => {
   const buttonLike = event.target;
   buttonLike.classList.toggle('place__icon-like_active');
 }
 
-const addCard = (cardData, inStart = true) => {
+const createCard = (cardData) => {
   const cardElement = cardTemplate.cloneNode(true);
   const cardPhoto = cardElement.querySelector('.place__photo');
   const cardTitle = cardElement.querySelector('.place__title');
   const cardButtonLike = cardElement.querySelector('.place__icon-like');
   const cardButtonDelete = cardElement.querySelector('.place__icon-delete');
 
-  cardPhoto.style.backgroundImage = `url(${cardData.link})`;
+  cardPhoto.src = cardData.link;
+  cardPhoto.alt = 'Фото, ' + cardData.name;
   cardTitle.textContent = cardData.name;
 
-  cardPhoto.addEventListener('click', openPopupShowCard);
+  cardPhoto.addEventListener('click', () => openPopupShowCard(cardData));
   cardButtonLike.addEventListener('click', toggleLike);
   cardButtonDelete.addEventListener('click', deleteCard);
+  return cardElement;
+}
 
-  if (inStart) gridCards.prepend(cardElement)
-  else gridCards.append(cardElement);
+const addCard = (cardData, inStart = true) => {
+  const cardElement = createCard(cardData);
+
+  if (inStart) cardsGrid.prepend(cardElement)
+  else cardsGrid.append(cardElement);
 }
 
 const deleteCard = (event) => {
@@ -109,14 +117,10 @@ const submitPopupEditProfile = (event) => {
   submitPopup(event, popupEditProfile);
 }
 
-const openPopupShowCard = (event) => {
-  const cardPhoto = event.target;
-  const cardTitle = cardPhoto.closest('.place').querySelector('.place__title');
-  const popupPhoto = popupShowCard.querySelector('.popup__photo');
-  const popupCaption = popupShowCard.querySelector('.popup__caption');
-
-  popupPhoto.src = cardPhoto.style.backgroundImage.split('url("')[1].slice(0, -2);
-  popupCaption.textContent = cardTitle.textContent;
+const openPopupShowCard = (cardData) => {
+  popupPhoto.src = cardData.link;
+  popupPhoto.alt = 'Фото, ' + cardData.name;
+  popupCaption.textContent = cardData.name;
   togglePopup(popupShowCard);
 }
 
@@ -137,7 +141,7 @@ initialCards.forEach(cardData => addCard(cardData, false));
 buttonEdit.addEventListener('click', openPopupEditProfile);
 buttonAdd.addEventListener('click', () => togglePopup(popupAddCard));
 // Add listeners to buttons that close popups
-buttonsClose.forEach(button => button.addEventListener('click', closePopup));
+closeButtons.forEach(button => button.addEventListener('click', closePopup));
 // Add listeners to buttons that submit popups
 buttonSubmitProfile.addEventListener('click', submitPopupEditProfile);
 buttonSubmitCard.addEventListener('click', submitPopupAddCard);
