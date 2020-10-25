2299c6503129b0b44ec0efa11ddb415db21fd347
Maria Komarova <komarova@phystech.edu>
2020-10-25 17:24:35 +0300

    fix: validate add-form

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,16 +1,21 @@
-const checkValidity = (formElement, inputElement, submitButton) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+const checkValidity = (inputElement, errorElement) => {
   if (!inputElement.validity.valid) {
     inputElement.classList.add('popup__input_type_error');
-    submitButton.classList.add('popup__submit-button_disabled');
-    submitButton.setAttribute('disabled', true);
     errorElement.textContent = inputElement.validationMessage;
   }
   else {
     inputElement.classList.remove('popup__input_type_error');
+    errorElement.textContent = '';
+  }
+}
+
+const checkValidityForSubmit = (inputElementsArray, submitButton) => {
+  if (inputElementsArray.some(inputElement => !inputElement.validity.valid)) {
+    submitButton.classList.add('popup__submit-button_disabled');
+    submitButton.setAttribute('disabled', true);
+  } else {
     submitButton.classList.remove('popup__submit-button_disabled');
     submitButton.removeAttribute('disabled', true);
-    errorElement.textContent = '';
   }
 }
 
@@ -19,13 +24,16 @@ const setEventListeners = (formElement) => {
   const submitButton = formElement.querySelector('.popup__submit-button');
 
   inputElementsArray.forEach(inputElement => {
-    inputElement.addEventListener('input', () => checkValidity(formElement, inputElement, submitButton))
+    inputElement.addEventListener('input', () => {
+      const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+      checkValidity(inputElement, errorElement);
+    });
   });
+  formElement.addEventListener('input', () => checkValidityForSubmit(inputElementsArray, submitButton));
 }
 
 const enableValidation = () => {
   const formElementsArray = Array.from(document.forms);
-  console.log(formElementsArray);
   formElementsArray.forEach(formElement => setEventListeners(formElement));
 }
 
